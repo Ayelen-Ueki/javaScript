40ae53059f18434293abacdc9408420cea7d7632
Maria Ayelen Ueki <aye_ueki_011@hotmail.com>
2021-12-03 01:43:43 -0300

    + calculo del total pedido

--- SegundaEntrega.js
+++ SegundaEntrega.js
@@ -1,7 +1,7 @@
 const antojos =
     [
         { id: 1, nombre: "alfajorcitos", precio: 500 },
-        { id: 1, nombre: "aRogel", precio: 500 },
+        { id: 1, nombre: "aRogel", precio: 600 },
         { id: 2, nombre: "cookies", precio: 300 }
     ]
 let carrito = [];
@@ -13,6 +13,12 @@ let botones = document.getElementsByClassName("botonCompra")
 for (let boton of botones) {
     boton.addEventListener("click", agregarCarrito)
 }
+
+for (let boton of botones) {
+    boton.addEventListener("click", calculototal)
+}
+
+// $(`.finCompra`).on("click", compraFinalizada);
 
 // $(`.botonCompra`).on("click", agregarCarrito);
 
@@ -27,7 +33,8 @@ function agregarCarrito() {
             icon: 'success',
             title: 'Gracias!',
             text: 'Has agregado un nuevo item al carrrito',
-            footer: '<a href="">Ver carrito</a>'
+            footer: '<a href="">Ver carrito</a>',
+
         })
     }
 
@@ -40,44 +47,55 @@ function agregarCarrito() {
             icon: 'success',
             title: 'Gracias!',
             text: 'Has agregado un nuevo item al carrrito',
-            footer: '<a href="">Ver carrito</a>'
+            footer: '<a href="">Ver carrito</a>',
         })
     }
     mostrarCarrito()
-    calculototal()
 }
 
 function mostrarCarrito() {
     let carritoGuardado = JSON.parse(localStorage.getItem("Carrito"))
     if (carritoGuardado) {
         let carritoActualizado = carritoGuardado[carritoGuardado.length - 1]
-        elementosCarrito.append(`
-    <div id="producto_${carritoActualizado.id}">
-        <ul>
-            <li>
-                <h3> Producto:${carritoActualizado.nombre}</h3>
-                <b> Precio: $${carritoActualizado.precio} </b>
-            </li>
-        </ul>
-    </div>
-        `)
+            elementosCarrito.append(`
+        <div id="producto_${carritoActualizado.id}">
+                <ul>
+                <li>
+                    <h3> Producto:${carritoActualizado.nombre}</h3>
+                    <b> Precio: $${carritoActualizado.precio} </b>
+                </li>
+            </ul>
+        </div>
+            `)
 
+        }
+}
+
+function calculototal()
+{
+    let carritoGuardado = JSON.parse(localStorage.getItem("Carrito"))
+    if (carritoGuardado.length>=1)
+    {
+        let compra = carritoGuardado.map(item => item.precio).reduce((prev, curr) => prev + curr, 0);
+        console.log(compra)
+        return compra
     }
 }
 
-// function calculototal()
-// {
-//     let carritoGuardado = JSON.parse(localStorage.getItem("Carrito"))
-//     if (carritoGuardado)
-//     {
-//         for (let monto of carritoGuardado)
-//         {
-//             monto = (carritoGuardado[carritoGuardado.length-1].precio) + (carritoGuardado[carritoGuardado.length-2].precio);
-//             console.log(monto)
-//         }     
-//     }
 
+
+// function compraFinalizada (){
+
+//      elementosCarrito.append(`
+//     <div>
+//         <h3> Total:${calculototal}</h3>
+//     </div>
+//         `)
 // }
+
+
+
+
 
 
 
